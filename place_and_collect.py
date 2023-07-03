2ee7291a28189971ba85c2b95eb45d92a6b77526
Makai Mann <makaim@cs.stanford.edu>
2023-07-02 21:50:00 -0400

    Pass the x value to generate data around

--- place_and_collect.py
+++ place_and_collect.py
@@ -1,7 +1,10 @@
+import argparse
 import csv
 import itertools
 import mss
 import numpy as np
+from pathlib import Path
+import sys
 import time
 from tqdm import tqdm
 
@@ -9,11 +12,16 @@ from xplane_autoland.xplane_connect.driver import XPlaneDriver
 from xplane_autoland.controllers.glideslope_controller import GlideSlopeController
 
 
-if __name__ == '__main__':
-    driver = XPlaneDriver()
-    driver.pause(True)
-    time.sleep(4)
-    print('Starting...')
+dphi_sweep   = np.arange(-5, 6, 1)
+dtheta_sweep = np.arange(-5, 6, 1)
+dpsi_sweep   = np.arange(-5, 6, 1)
+dx_sweep     = np.arange(-100, 101, 50)
+dy_sweep     = np.arange(-100, 101, 50)
+dh_sweep     = np.arange(-100, 101, 50)
+
+
+def data_for_x(driver, x_center):
+    print(f"Saving data for x={x_center}")
     gsc    = GlideSlopeController(gamma=3)
 
     h_thresh = gsc._h_thresh
@@ -21,65 +29,75 @@ if __name__ == '__main__':
     slope = float(start_elev - h_thresh) / driver._start_ground_range
 
     save_dir = 'data'
-    f = open(f'./{save_dir}/states.csv', 'w')
+    statepath = Path(f"./{save_dir}/states.csv")
+    if not statepath.is_file():
+        with open(str(statepath), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['phi', 'theta', 'psi', 'x', 'y', 'h', 'imagename'])
+
+    f = open(str(statepath), 'a')
     writer = csv.writer(f)
-    writer.writerow(['phi', 'theta', 'psi', 'x', 'y', 'h', 'imagename'])
-    imgid = 0
     sct = mss.mss()
-
-    # parameter sweeps
-    x_sweep      = np.arange(0., driver._start_ground_range, 1.)
-
-    # dphi_sweep   = np.arange(-5, 6, 5)
-    # dtheta_sweep = np.arange(-5, 6, 5)
-    # dpsi_sweep   = np.arange(-5, 6, 5)
-    dphi_sweep   = [0]
-    dtheta_sweep = [0]
-    dpsi_sweep   = [0]
-    dx_sweep     = np.arange(-100, 101, 100)
-    dy_sweep     = np.arange(-100, 101, 100)
-    dh_sweep     = np.arange(-100, 101, 100)
-
 
     try:
         # sample points around the glideslope
-        for x in tqdm(np.arange(0., 12464., 100.)):
-            driver.reset()
+        h = slope * x_center + h_thresh
+        for dphi, dtheta, dpsi, dx, dy, dh in itertools.product(dphi_sweep,
+                                                                dtheta_sweep,
+                                                                dpsi_sweep,
+                                                                dx_sweep,
+                                                                dy_sweep,
+                                                                dh_sweep):
+            if h+dh < h_thresh:
+                continue
+            elif x_center+dx < 0:
+                continue
+
             # set time to 8am
             driver._client.sendDREF("sim/time/zulu_time_sec", 8 * 3600 + 8 * 3600)
-            h = slope * x + h_thresh
-            print(f'Base: x={x}, h={h}')
-            for dphi, dtheta, dpsi, dx, dy, dh in itertools.product(dphi_sweep,
-                                                                    dtheta_sweep,
-                                                                    dpsi_sweep,
-                                                                    dx_sweep,
-                                                                    dy_sweep,
-                                                                    dh_sweep):
-                if h+dh < h_thresh:
-                    continue
-                elif x+dx < 0:
-                    continue
-                orient_pos = [dphi, dtheta, dpsi, x+dx, dy, h+dh]
-                driver.set_orient_pos(*orient_pos)
-                state = driver.get_statevec()
-                actual_orient_pos = state[-6:]
-                abs_diff = np.abs(orient_pos - actual_orient_pos)
-                rel = np.abs(orient_pos)
-                rel[rel < 1] = 1.
-                per_diff = abs_diff / rel
-                if not np.all(per_diff < 0.05):
-                    print(f'Warning: more than 5% difference between requested and actual state')
-                    print(f'% Diff: {per_diff}')
-                    print(f'Requested: {orient_pos}')
-                    print(f'Actual: {actual_orient_pos}')
-                nv_pairs = zip(['phi', 'theta', 'psi', 'x', 'y', 'h'], actual_orient_pos)
-                statestr = '_'.join([p0 + str(int(p1)) for p0, p1 in nv_pairs])
-                fname = f'./{save_dir}/images/image_{statestr}.png'
-                sct.shot(mon=1, output=fname)
-                phi, theta, psi, x, y, h = actual_orient_pos
-                writer.writerow([phi, theta, psi, x, y, h, fname])
-                time.sleep(0.001)
-                imgid += 1
-            print(f"Last image: {imgid-1}")
+
+            orient_pos = [dphi, dtheta, dpsi, x_center+dx, dy, h+dh]
+            driver.set_orient_pos(*orient_pos)
+            state = driver.get_statevec()
+            actual_orient_pos = state[-6:]
+            abs_diff = np.abs(orient_pos - actual_orient_pos)
+            rel = np.abs(orient_pos)
+            rel[rel < 1] = 1.
+            per_diff = abs_diff / rel
+            if not np.all(per_diff < 0.05):
+                print(f'Warning: more than 5% difference between requested and actual state')
+                print(f'% Diff: {per_diff}')
+                print(f'Requested: {orient_pos}')
+                print(f'Actual: {actual_orient_pos}')
+            nv_pairs = zip(['phi', 'theta', 'psi', 'x', 'y', 'h'], actual_orient_pos)
+            statestr = '_'.join([p0 + str(int(p1)) for p0, p1 in nv_pairs])
+            fname = f'./{save_dir}/images/image_{statestr}.png'
+            sct.shot(mon=1, output=fname)
+            phi, theta, psi, x, y, h = actual_orient_pos
+            writer.writerow([phi, theta, psi, x, y, h, fname])
+            time.sleep(0.001)
     except KeyboardInterrupt:
         print('Interrupted.', flush=True)
+
+    f.close()
+
+
+def sweep_x(driver):
+    # parameter sweeps
+    x_sweep      = np.arange(0., driver._start_ground_range, 100.)
+    for x_center in range(x_sweep):
+        data_for_x(x_center)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Save data for training a vision-based state estimator")
+    parser.add_argument("x_center", type=float, help="Which x value to collect data around")
+    args = parser.parse_args()
+
+    driver = XPlaneDriver()
+    driver.pause(True)
+    driver.reset()
+    time.sleep(4)
+    print('Starting...')
+
+    data_for_x(driver, args.x_center)
