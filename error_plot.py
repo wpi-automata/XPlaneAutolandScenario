749173407505fc220a7fe1b5046178d0de780010
cscott <cbscott@wpi.edu>
2023-12-06 12:42:52 -0500

    Added 3D Plotting

--- error_plot.py
+++ error_plot.py
@@ -40,117 +40,142 @@ def plot_data(repo_dir):
     #Eventually should save plots as files 
     # TODO Change plots to scatter and plot 3D against distance to runway(x)
 
-    #X_Error vs X
-    plt.figure(1)
+    #X is horizontal distance from plane to runway
+    #Y is cross-track error 
+    #All 3D Plots are plotted with NN Error on the Z Axis and Horizontal Distance (X) on the X-axis
+
+    #X_Error (Horizontal Distance Error) vs X (Horizontal Distance), 2D plot
+    plt.figure("Figure 1")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_x, df.x)
-    plt.xlabel("NN Error X")
-    plt.ylabel("X")
+    # ax = fig1.add_subplot(projection = "3d")
 
-    #X_Error vs Y
-    plt.figure(2)
+    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
+    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", skiprows=lambda x: x%10, usecols=columns)
+
+    plt.scatter(df.error_x, df.x)
+    plt.title("Horizontal Distance Error (NN) vs Horizontal Distance (true x)")
+    plt.ylabel("Horizontal Distance (True x)")
+    plt.xlabel("Horizontal Distance Error (Predicted x)")
+
+    #3D PLOTS =========================================================================================
+    #X_Error (Horizontal Distance Error) vs Y (Crosstrack Error)
+    fig2 = plt.figure("Figure 2")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_x, df.y)
-    plt.xlabel("NN Error X")
-    plt.ylabel("Y")
- 
+    ax = fig2.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.y, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Crosstrack vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Crosstrack Error (Y)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
 
-    #X_Error vs H
-    plt.figure("3")
+    # #X_Error (Horizontal Distance Error) vs Height 
+    fig3 = plt.figure("Figure 3")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_x, df.h)
-    plt.xlabel("NN Error X")
-    plt.ylabel("H")
+    ax = fig3.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.h, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Height vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Height (y)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
 
 
-    #X_Error vs Phi
-    plt.figure("4")
+    # #X_Error (Horizontal Distance Error) vs Phi (Roll)
+    fig4 = plt.figure("Figure 4")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_x, df.phi)
-    plt.xlabel("NN Error X")
-    plt.ylabel("Phi")
+    ax = fig4.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.phi, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Roll (Phi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Roll (Phi)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
 
 
-    #X_Error vs Theta
-    plt.figure("5")
+
+    # X_Error (Horizontal Distance Error) vs Theta (Pitch)
+    fig5 = plt.figure("Figure 5")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_x, df.theta)
-    plt.xlabel("NN Error X")
-    plt.ylabel("Theta")
+    ax = fig5.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.theta, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Pitch (Theta) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Theta")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
 
 
-    #X_Error vs Psi
-    plt.figure("6")
+    # #X_Error (Horizontal Distance Error) vs Psi (yaw)
+    fig6 = plt.figure("Figure 6")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_x, df.psi)
-    plt.xlabel("NN Error X")
-    plt.ylabel("Psi")
+    ax = fig6.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.psi, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Yaw (psi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Psi (yaw)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
     
 
-    #Y_Error vs X
-    plt.figure("7")
+    #Y_Error (Crosstrack error) vs X (Horizontal Distance), 2D plot
+    plt.figure("Figure 7")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_y, df.x)
-    plt.xlabel("NN Error Y")
-    plt.ylabel("X")
-  
 
-    #Y_Error vs Y
-    plt.figure("8")
+    plt.scatter(df.error_y, df.x)
+    plt.title("Crosstrack Error (NN) vs Horizontal Distance (true x)")
+    plt.ylabel("Horizontal Distance (True x)")
+    plt.xlabel("Crosstrack Error(Predicted y)")
+
+    #3D PLOTS =========================================================================================
+    #Y_Error (Crosstrack error) vs Y (Crosstrack Error)
+    fig8 = plt.figure("Figure 8")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_y, df.y)
-    plt.xlabel("NN Error Y")
-    plt.ylabel("Y")
+    ax = fig8.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.y, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Crosstrack vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Crosstrack Error (Y)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
 
-
-    #Y_Error vs H
-    plt.figure("9")
+    #Y_Error (Crosstrack error) vs Height
+    fig9 = plt.figure("Figure 9")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_y, df.h)
-    plt.xlabel("NN Error Y")
-    plt.ylabel("H")
-  
+    ax = fig9.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.h, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Height vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Height")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
 
-    #Y_Error vs Phi
-    plt.figure("10")
+
+    # Y_Error (Crosstrack error) vs Phi (Roll)
+    fig10 = plt.figure("Figure 10")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_y, df.phi)
-    plt.xlabel("NN Error Y")
-    plt.ylabel("Phi")
-    plt.xlabel("NN Error Y")
-    plt.ylabel("Theta")
-   
+    ax = fig10.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.phi, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Roll (Phi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Roll (Phi)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
 
 
-    #Y_Error vs Psi
-    plt.figure("12")
+
+    # Y_Error (Crosstrack error) vs Theta (Pitch)
+    fig11 = plt.figure("Figure 11")
     plt.rcParams["figure.autolayout"] = True
-    columns = ["error_x", "error_y","phi", "theta", "psi", "x", "y", "h"]
-    df = pd.read_csv(f"{repo_dir}/plots/2023-11-27/combined_states.csv", usecols=columns)
-    plt.plot(df.error_y, df.psi)
-    plt.xlabel("NN Error Y")
-    plt.ylabel("Psi")
+    ax = fig11.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.theta, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Roll (Phi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Pitch (theta)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
 
+
+    # Y_Error (Crosstrack error) vs Psi (yaw)
+    fig12 = plt.figure("Figure 12")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig12.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.psi, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Roll (Phi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Yaw (psi)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
 
     plt.show()
 
