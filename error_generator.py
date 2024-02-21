49f51f4320367f30801d2912dfe77cc8f0aa3490
cscott <cbscott@wpi.edu>
2024-02-21 00:28:42 -0500

    Updated model and rewrote script to match most up-to-date autoland

---
+++ error_generator.py
@@ -0,0 +1,73 @@
+import argparse
+import csv
+from datetime import date
+from pathlib import Path
+import torch 
+
+from src.xplane_autoland.xplane_connect.vision_driver import XPlaneVisionDriver
+from src.xplane_autoland.vision.perception import AutolandPerceptionModel
+from src.xplane_autoland.vision.perception import AutolandPerceptionModel
+from src.xplane_autoland.vision.xplane_data import AutolandImageDataset
+from src.xplane_autoland.controllers.glideslope_controller import GlideSlopeController
+
+def generate_data(plane, dataloader, save_dir):
+    gsc = GlideSlopeController(gamma=3)
+    print(f"Attempting to save data...")
+    statepath = Path(f"{save_dir}/generated_states.csv")
+    if not statepath.is_file():
+        with open(str(statepath), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['y_err', 'h_err_NN','h_err_true'])
+
+    f = open(str(statepath), 'a')
+    writer = csv.writer(f)
+
+    #Iterate over the dataset
+    #processed states values for reference: phi,theta,psi,x,y,h,imagename
+    print(f"Iterating over the data")
+    with open(f"{repo_dir}/data/processed-states.csv") as states_file: 
+        csv_reader = csv.reader(states_file, delimiter=',')
+        for row, (rwy_img, orient_alt, _ ) in zip(csv_reader, dataloader):
+    
+            #iterate over the data and pass it into est_state
+            y_err, h_err = plane.est_pos_state(rwy_img, orient_alt)
+            h_err_true = gsc.get_glideslope_height_at(float(row[3])) - float(row[5])
+
+            #return the est_state values in the x and y and save them to a csv file 
+            writer.writerow([y_err, h_err, h_err_true])
+        
+    print(f"Done")
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description="Run pre-generated data through the state estimator")
+    parser.add_argument("--save-dir", help="The directory to save everything in", default=None)
+    parser.add_argument("--data-dir", help="The directory with image data should contain a states.csv and images directory", default=None)
+    parser.add_argument("--resnet-version", help="Choose which resnet to use", default="50", choices=["18", "50"])
+    parser.add_argument("--model", help="The path to model parameters (*.pt) for a vision network. Note must have XPlane fullscreen for screenshots", default=None)
+    args = parser.parse_args()
+
+    this_dir = Path(__file__).parent
+    repo_dir = this_dir.resolve()
+
+    today = date.today()
+    save_dir = Path(f"{repo_dir}/errors/{today.year}-{today.month}-{today.day}")
+    if not save_dir.exists():
+        save_dir.mkdir()
+
+    model = AutolandPerceptionModel(resnet_version="50")
+    model.load("/home/colette/XPlaneAutolandScenario/models/2023-12-6/best_model_params.pt")
+    model.eval()
+    plane = XPlaneVisionDriver(model)
+    
+    #1: Collect the Dataset 
+    data_dir = args.data_dir
+    if data_dir is None:
+        data_dir=str(repo_dir/"data")
+    dataset = AutolandImageDataset(f"{data_dir}/processed-states.csv", f"{data_dir}/images", transform=model.preprocess)
+
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4)
+
+    #2: Generate Data
+    generate_data(plane, dataloader, save_dir)
---
+++ error_plot.py
@@ -0,0 +1,189 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
+import csv
+from datetime import date
+import pandas as pd
+
+def combine_data(save_dir, repo_dir):
+
+    statepath = Path(f"{save_dir}/combined_states.csv")
+    if not statepath.is_file():
+        with open(str(statepath), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['error_h', 'error_y','phi', 'theta', 'psi', 'x', 'y', 'h'])
+
+    f = open(str(statepath), 'a')
+    writer = csv.writer(f)
+
+    with open(f"{repo_dir}/data/processed-states.csv") as states_file: #This needs to change 
+        with open(f"{repo_dir}/errors/2024-1-15/generated_states.csv") as gen_states_file:
+            csv_reader1 = csv.reader(states_file, delimiter=',')
+            csv_reader2 = csv.reader(gen_states_file, delimiter=',')
+            line_count = 0
+            for row1, row2 in zip(csv_reader1, csv_reader2):
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    error_h = float(row2[1]) - float(row2[2])
+                    error_y = float(row1[4]) - float(row2[0])
+                    phi = row1[0]
+                    theta = row1[1]
+                    psi = row1[2]
+                    x = row1[3]
+                    y = row1[4]
+                    h = row1[5]
+                    writer.writerow([error_h, error_y, phi, theta, psi, x, y, h])
+                    line_count += 1
+            print(f'Processed {line_count} lines.')
+
+def plot_data(repo_dir):
+    #Eventually should save plots as files 
+    # TODO Change plots to scatter and plot 3D against distance to runway(x)
+
+    #X is horizontal distance from plane to runway
+    #Y is cross-track error 
+    #All 3D Plots are plotted with NN Error on the Z Axis and Horizontal Distance (X) on the X-axis
+
+    #X_Error (Horizontal Distance Error) vs X (Horizontal Distance), 2D plot
+    plt.figure("Figure 1")
+    plt.rcParams["figure.autolayout"] = True
+    # ax = fig1.add_subplot(projection = "3d")
+
+    columns = ["error_h", "error_y","phi", "theta", "psi", "x", "y", "h"]
+    df = pd.read_csv(f"{repo_dir}/plots/2024-1-15/combined_states.csv", skiprows=lambda x: x%10, usecols=columns)
+
+    plt.scatter(df.error_h, df.x)
+    plt.title("Height Error (NN) vs Height (True h)")
+    plt.ylabel("Height(True h)")
+    plt.xlabel("Height Error (Predicted H)")
+
+    #3D PLOTS =========================================================================================
+    #X_Error (Horizontal Distance Error) vs Y (Crosstrack Error)
+    fig2 = plt.figure("Figure 2")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig2.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.y, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Crosstrack vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Crosstrack Error (Y)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
+
+    # #X_Error (Horizontal Distance Error) vs Height 
+    fig3 = plt.figure("Figure 3")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig3.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.h, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Height vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Height (y)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
+
+
+    # #X_Error (Horizontal Distance Error) vs Phi (Roll)
+    fig4 = plt.figure("Figure 4")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig4.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.phi, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Roll (Phi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Roll (Phi)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
+
+
+
+    # X_Error (Horizontal Distance Error) vs Theta (Pitch)
+    fig5 = plt.figure("Figure 5")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig5.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.theta, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Pitch (Theta) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Theta")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
+
+
+    # #X_Error (Horizontal Distance Error) vs Psi (yaw)
+    fig6 = plt.figure("Figure 6")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig6.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.psi, df.error_x)
+    plt.title("Horizontal Distance Error (NN) vs Yaw (psi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Psi (yaw)")
+    ax.set_zlabel("Horizontal Distance Error (Predicted X)")
+    
+
+    #Y_Error (Crosstrack error) vs X (Horizontal Distance), 2D plot
+    plt.figure("Figure 7")
+    plt.rcParams["figure.autolayout"] = True
+
+    plt.scatter(df.error_y, df.x)
+    plt.title("Crosstrack Error (NN) vs Horizontal Distance (true x)")
+    plt.ylabel("Horizontal Distance (True x)")
+    plt.xlabel("Crosstrack Error(Predicted y)")
+
+    #3D PLOTS =========================================================================================
+    #Y_Error (Crosstrack error) vs Y (Crosstrack Error)
+    fig8 = plt.figure("Figure 8")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig8.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.y, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Crosstrack vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Crosstrack Error (Y)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
+
+    #Y_Error (Crosstrack error) vs Height
+    fig9 = plt.figure("Figure 9")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig9.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.h, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Height vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Height")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
+
+
+    # Y_Error (Crosstrack error) vs Phi (Roll)
+    fig10 = plt.figure("Figure 10")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig10.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.phi, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Roll (Phi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Roll (Phi)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
+
+
+
+    # Y_Error (Crosstrack error) vs Theta (Pitch)
+    fig11 = plt.figure("Figure 11")
+    plt.rcParams["figure.autolayout"] = True
+    ax = fig11.add_subplot(projection = "3d")
+    ax.scatter( df.x, df.theta, df.error_y)
+    plt.title("Crosstrack Error (NN) vs Roll (Phi) vs Horizontal Distance")
+    ax.set_xlabel("Horizontal Distance (True X)")
+    ax.set_ylabel("Pitch (theta)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
+
+
+    # Y_Error (Crosstrack error) vs Psi (yaw)
+    fig12 = plt.figure("Figure 12")
+    ax.set_ylabel("Yaw (psi)")
+    ax.set_zlabel("Crosstrack Error (NN, Pred Y)")
+
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    this_dir = Path(__file__).parent
+    repo_dir = this_dir.resolve()
+
+    today = date.today()
+    save_dir = Path(f"{repo_dir}/plots/{today.year}-{today.month}-{today.day}")
+    if not save_dir.exists():
+        save_dir.mkdir()
+    
+    combine_data(save_dir, repo_dir)
+    #plot_data(repo_dir)
