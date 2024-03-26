9a046c6e100875f6b43f612b7122cade9a6f8076
cscott <cbscott@wpi.edu>
2024-03-25 22:09:43 -0400

    added in functionality for collecting script data

--- run_autoland.py
+++ run_autoland.py
@@ -3,6 +3,9 @@ import argparse
 import math
 import sys
 import time
+from datetime import date
+from pathlib import Path
+import csv
 
 from src.xplane_autoland.controllers.glideslope_controller import GlideSlopeController
 from src.xplane_autoland.xplane_connect.vision_driver import XPlaneVisionDriver
@@ -13,6 +16,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run the autoland scenario at KMWH Grant County International Airport Runway 04. You must start XPlane and choose the airport + a Cessna separately.")
     parser.add_argument("--model", help="The path to model parameters (*.pt) for a vision network. Note must have XPlane fullscreen for screenshots", default=None)
     args = parser.parse_args()
+
+    this_dir = Path(__file__).parent
+    repo_dir = this_dir.resolve()
+
+    today = date.today()
+    save_dir = Path(f"{repo_dir}/autoland_errors/{today.year}-{today.month}-{today.day}")
+    if not save_dir.exists():
+        save_dir.mkdir()
+
+    statepath = Path(f"{save_dir}/Trial15-12464_states.csv")
+    if not statepath.is_file():
+        with open(str(statepath), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['phi, theta, psi, x, y, y_pred, h, h_err, h_err_pred, y_err_NN, h_err_NN'])
+
+    f = open(str(statepath), 'a')
+    writer = csv.writer(f)
 
     WITH_VISION = False
     if args.model:
@@ -38,7 +58,7 @@ if __name__ == '__main__':
     # distance from the runway crossing (decrease to start closer)
     # vision mode works best at 9000m and less (up until right before landing)
     # need to train more at higher distances and close up
-    x_val = 9000
+    x_val = 12464
     init_h = slope * x_val + h_thresh
     # can set the state arbitrarily (see reset documentation for semantics)
     plane.reset(init_x=x_val, init_h=init_h)
@@ -49,17 +69,25 @@ if __name__ == '__main__':
         for step in range(math.ceil(max_time/dt)):
             state = plane.get_statevec()
             phi, theta, psi, x, y, h = state[-6:]
-            err_h = None
+            print(x)
+            h_err_pred = None
 
             if WITH_VISION:
                 plane.pause(True)
-                y, err_h = plane.est_pos_state()
+                y_pred, h_err_pred = plane.est_pos_state()
                 # update state vector with y estimate
                 # err_h will be used directly
-                state[-2] = y
+                state[-2] = y_pred
+
+                #Calculations to get NN Errors and true height error
+                h_err = gsc.get_glideslope_height_at(x) - h
+                y_err_NN = y - y_pred
+                h_err_NN = h_err - h_err_pred
+
+                writer.writerow([phi, theta, psi, x, y, y_pred, h, h_err, h_err_pred, y_err_NN, h_err_NN])
                 # print("Y: %f", y_1)
 
-            elevator, aileron, rudder, throttle = gsc.control(state, err_h=err_h)
+            elevator, aileron, rudder, throttle = gsc.control(state, err_h=h_err_pred)
             # the runway slopes down so this works fine
             if h <= gsc.runway_elevation and x <= 0:
                 # disable throttle once you've landed
