432e4e6041f46aa8b08a7bcedb533a100e84b218
AGChad <agchadbourne@wpi.edu>
2024-06-21 09:04:55 -0400

    begin storing ood_signal and validation set

--- wpi_scod.py
+++ wpi_scod.py
@@ -9,6 +9,8 @@ import torch
 from torch.utils.data import DataLoader
 import numpy as np
 from tqdm import trange
+import csv
+from pathlib import Path
 
 from matplotlib import pyplot as plt
 from src.xplane_autoland.vision.perception import AutolandPerceptionModel
@@ -30,22 +32,28 @@ dist_layer = scod.distributions.NormalMeanParamLayer()
 
 scod_model = scod.SCOD(model)
 
-data_dir = "/media/storage_drive/ULI Datasets/OOD Data/dataWPI_50-10"
+data_dir = "/media/storage_drive/ULI Datasets/OOD Data/dataWPI_200-15"
 full_dataset = AutolandImageDataset(f"{data_dir}/states.csv", f"{data_dir}/images")
-train_size = int(0.01 * len(full_dataset))
+train_size = int(0.8 * len(full_dataset))
 val_size = len(full_dataset) - train_size
 train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
 # might need to experiment with different values for "batch_size" and "lr"
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
-rwy_img, orient_alt, labels  = next(iter(train_dataloader))
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=val_size, shuffle=True)
+
 # optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
 
 print(type(train_dataset.dataset.img_labels))
-scod_model.process_dataset(train_dataset, dist_layer)
+#scod_model.process_dataset(train_dataset, dist_layer)
+#torch.save(scod_model.state_dict(), "/home/achadbo/XPlaneAutolandScenario/models/scod/2024-6-18/scod_model_200_80percent.pt")
 
-torch.save(scod_model, "/home/achadbo/XPlaneAutolandScenario/models/scod/2024-6-18")
+statepath = Path(f"/home/achadbo/XPlaneAutolandScenario/Subsets/scod_200")
+if not statepath.is_file():
+    with open(str(statepath), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['phi', 'theta', 'psi', 'x', 'y', 'h', 'img source'])
 
+rwy_imgs, orient_alts, labels  = next(iter(val_dataloader))
 ood_detector = scod.OodDetector(scod_model, dist_layer)
-ood_signal = ood_detector(rwy_img, orient_alt)
-print(ood_signal)
+# ood_signal = ood_detector(rwy_img, orient_alt)
+# print(ood_signal)
 
