68745745a2e23b6a74f0e3a38c77eaa6f735daab
WaliaRohan <walia.rohan@gmail.com>
2024-10-05 17:07:00 -0400

    Removed hardcoded adding of xplane_autoland folder

--- src/xplane_autoland/xplane_connect/vision_driver.py
+++ src/xplane_autoland/xplane_connect/vision_driver.py
@@ -1,5 +1,12 @@
+import os
 import sys
-sys.path.append('/home/colette/XPlaneAutolandScenario/src/xplane_autoland')
+
+current_dir = os.getcwd()
+subdirectory = os.path.join(current_dir, 'src', 'xplane_autoland')
+
+sys.path.append(subdirectory)
+
+
 from xplane_connect.driver import XPlaneDriver
 
 from vision.perception import AutolandPerceptionModel
@@ -28,11 +35,7 @@ class XPlaneVisionDriver(XPlaneDriver):
         # Need to stop and take image
         self._sct = mss.mss()
 
-<<<<<<< HEAD
-    def est_pos_state(self, monitor):
-=======
     def est_pos_state(self, img, orient_alt): #Refactor to take in image and orientation data for error data generation
->>>>>>> origin/ava-error-dev
         """
         Estimates the position state using a vision network
 
@@ -40,19 +43,7 @@ class XPlaneVisionDriver(XPlaneDriver):
             y -- lateral deviation
             err_h -- error in height relative to a glideslope
         """
-<<<<<<< HEAD
-        sct_img = self._sct.grab(self._sct.monitors[monitor]) 
-        pil_img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-        img = to_tensor(pil_img)
-        img = self._state_estimator.preprocess(img)
-        phi, theta, psi = self.est_orient_state()
-        _, _, h  = self.get_pos_state()
-        orient_alt = torch.FloatTensor([phi, theta, psi, h])
-        orient_alt /= self._orient_norm_divisor
-        img, orient_alt = img[None, :, :, :], orient_alt[None, :]
-=======
         #Take out pre-processing and image generation (already done)
->>>>>>> origin/ava-error-dev
 
         # sct_img = self._sct.grab(self._sct.monitors[1])
         # pil_img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
@@ -73,24 +64,14 @@ class XPlaneVisionDriver(XPlaneDriver):
             y_err *= label_mult
             h_err *= label_mult
             #Debug statements 
-<<<<<<< HEAD
-            # print("Got here")
-            # print("Y: %f", y_err)
-            # print("H: %f", h_err)
-=======
             # print("Got here") 
             # print("Y: %f", y_err)
->>>>>>> origin/ava-error-dev
 
         return y_err.item(), h_err.item()
 
 
 if __name__ == "__main__":
     model = AutolandPerceptionModel(resnet_version="50")
-<<<<<<< HEAD
-    model.load("/home/colette/XPlaneAutolandScenario/models/2024-3-3/best_model_params.pt")
-=======
     model.load("/home/agchadbo/XPlaneAutolandScenario/models/2023-12-6/best_model_params.pt")
->>>>>>> origin/ava-error-dev
     model.eval()
     vision_driver = XPlaneVisionDriver(model)
